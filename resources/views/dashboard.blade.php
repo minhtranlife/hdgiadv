8768ac393088e55066125c9b2f316c9995cf83c2
MinhTran <minhtranlife@gmail.com>
2016-06-06 11:24:00 +0700

    Fix

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -10,12 +10,16 @@
 @stop
 
 @section('content')
+    <div class="container">
     <div class="page-content">
-        <div class="container">
-            <div class="row">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="text-center">
                     <img src="{{url('data/uploads/mohinh/MoHinhGiaDv.jpg')}}" alt="" class="img-responsive pull-center mrm" />
+                </div>
             </div>
         </div>
+    </div>
     </div>
     <!--END CONTENT--></div>
 @stop 
