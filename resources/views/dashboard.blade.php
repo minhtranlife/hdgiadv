fa07aa7a3dd293f2eb5b018d1c959f80915c55f0
MinhTran <minhtranlife@gmail.com>
2016-06-06 11:13:53 +0700

    Update MoHinh

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -11,12 +11,12 @@
 
 @section('content')
     <div class="page-content">
+        <div class="container">
         <div class="row">
             <div class="col-lg-12">
-                <div class="text-center">
-                    <img src="{{url('data/uploads/mohinh/MoHinhGiaDv.jpg')}}" alt="" class="img-responsive pull-center mrm" />
-                </div>
+                <img src="{{url('data/uploads/mohinh/MoHinhGiaDv.jpg')}}" alt="" class="img-responsive pull-center mrm" />
             </div>
+        </div>
         </div>
     </div>
     <!--END CONTENT--></div>
