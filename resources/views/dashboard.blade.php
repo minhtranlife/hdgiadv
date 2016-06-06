41e42110b4b834d5a08da32263dd45e5b7e3f44c
MinhTran <minhtranlife@gmail.com>
2016-06-06 11:15:58 +0700

    Fix

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -12,11 +12,9 @@
 @section('content')
     <div class="page-content">
         <div class="container">
-        <div class="row">
-            <div class="col-lg-12">
-                <img src="{{url('data/uploads/mohinh/MoHinhGiaDv.jpg')}}" alt="" class="img-responsive pull-center mrm" />
+            <div class="row">
+                    <img src="{{url('data/uploads/mohinh/MoHinhGiaDv.jpg')}}" alt="" class="img-responsive pull-center mrm" />
             </div>
-        </div>
         </div>
     </div>
     <!--END CONTENT--></div>
